5b2fb77f1436560211724a1e82f33299e14a3fe0
Elaine-Smit <elaine.smit8@gmail.com>
2022-10-05 14:11:11 +0200

    Header
    Added nav and a some styling

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,12 +1,28 @@
-// import logo from './logo.svg';
+// import headBackground from './images/keyboard_2.jpg';
 import './App.css';
+import Intro from "./components/Intro.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <nav>
+          <ul className="nav">
+            <li><a href="#intro" alt="home link">Introduction</a></li>
+            <li><a href="#services" alt="services link">What I do</a></li>
+            <li><a href="#tech" alt="technologies link">What I use</a></li>
+            <li><a href="#contact" alt="contact link">Contact</a></li>
+          </ul>
+        </nav>
       </header>
+      <main>
+        <div className="container-main">
+          <Intro />
+        </div>
+      </main>
+      <footer>
+
+      </footer>
     </div>
   );
 }
---
+++ my-portfolio/src/components/Intro.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Introduction(){
+    return (
+        <div className="main">
+            <div className="intro">
+                <h1 id="intro">Elaine Smit</h1>
+                <br/>
+                <h2>Front-End Dev</h2>
+            </div>
+            <div className="main-body">
+
+            </div>
+        </div>
+    )
+    
+}
