08746680c15664708428e0404a831565fe58aeaf
Elaine-Smit <elaine.smit8@gmail.com>
2022-10-05 14:41:32 +0200

    Body
    Added intro and some more styling

--- my-portfolio/src/components/Intro.js
+++ my-portfolio/src/components/Intro.js
@@ -1,4 +1,5 @@
 import React from "react"
+import DecImage from "../images/draw_1.jpg"
 
 export default function Introduction(){
     return (
@@ -9,7 +10,14 @@ export default function Introduction(){
                 <h2>Front-End Dev</h2>
             </div>
             <div className="main-body">
-
+                <div className="what-i-do">
+                    <h1>What I do <small>...for now</small></h1>
+                    
+                    <img className="dec-img" src={DecImage} alt=""/>
+                </div>
+                <div className="services">
+                    <h1>Services</h1>
+                </div>
             </div>
         </div>
     )
