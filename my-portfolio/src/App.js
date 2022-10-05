7936882bf67081668c47c3b607bc943c072ab6e9
Elaine-Smit <elaine.smit8@gmail.com>
2022-10-05 17:51:21 +0200

    Added work section
    Basic design only

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,7 +1,7 @@
 // import headBackground from './images/keyboard_2.jpg';
 import './App.css';
 import Intro from "./components/Intro"
-import Work from "./components/Work"
+import Use from "./components/Use"
 
 function App() {
   return (
@@ -9,7 +9,6 @@ function App() {
       <header className="App-header">
         <nav>
           <ul className="nav">
-            <li><a href="#intro" alt="home link">Introduction</a></li>
             <li><a href="#services" alt="services link">What I do</a></li>
             <li><a href="#tech" alt="technologies link">What I use</a></li>
             <li><a href="#contact" alt="contact link">Contact</a></li>
@@ -20,7 +19,7 @@ function App() {
         <div className="container-main">
           <Intro />
           <br/>
-          <Work />
+          <Use />
         </div>
       </main>
       <footer>
---
+++ my-portfolio/src/components/Use.js
@@ -0,0 +1,50 @@
+import React from "react"
+
+export default function Use(){
+    return(
+        <div className="work-container" id="tech">
+            <div className="work-head">What I use</div>
+            <div className="work-sub">Here a list of some of the languages, frameworks, software I use.</div>
+            <div className="">
+                <h3>JavaScript</h3>
+            </div>
+            <div className="">
+                <h3>HTML</h3>
+                <h5>What I use it for</h5>
+                <p>Design.</p>
+            </div>
+            <div className="">
+                <h3>CSS</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>C#</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>React</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>.NET</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>Illustrator</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>PhotoShop</h3>
+                <p></p>
+            </div>
+            <div className="">
+                <h3>Netlify</h3>
+                <p></p>
+            </div>
+            <div className="work-footer">
+                <h3>Footer</h3>
+                <p></p>
+            </div>
+        </div>
+    )
+}
