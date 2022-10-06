e156a63c166c999312b64dee956cee9a57b725bc
Elaine-Smit <elaine.smit8@gmail.com>
2022-10-06 09:37:08 +0200

    What I use
    Added logos and styling

--- my-portfolio/src/components/Use.js
+++ my-portfolio/src/components/Use.js
@@ -1,4 +1,13 @@
 import React from "react"
+import HTML from "../images/html-logo.png"
+import CSS from "../images/css-logo.png"
+import JS from "../images/js-logo.png"
+import CSharp from "../images/cSharp.svg"
+import reactLogo from "../images/react-logo.png"
+import dotNet from "../images/NET_Core_Logo.png"
+import Adobe from "../images/adobe-suite-logo.png"
+import Bootstrap from "../images/bootstrap-logo-vector.svg"
+import Node from "../images/node-logo.png"
 
 export default function Use(){
     return(
@@ -7,39 +16,42 @@ export default function Use(){
             <div className="work-sub">Here a list of some of the languages, frameworks, software I use.</div>
             <div className="">
                 <h3>JavaScript</h3>
+                <img src={JS}  alt="JS logo" className="js logo"/>
+                <p>Design.</p>
             </div>
             <div className="">
                 <h3>HTML</h3>
-                <h5>What I use it for</h5>
+                <img src={HTML}  alt="HTML logo" className="html logo"/>
                 <p>Design.</p>
             </div>
             <div className="">
                 <h3>CSS</h3>
-                <p></p>
+                <img src={CSS}  alt="CSS logo" className="css logo"/>
+                <p>Design.</p>
             </div>
             <div className="">
                 <h3>C#</h3>
-                <p></p>
+                <img src={CSharp}  alt="CSS logo" className="cSharp logo"/>
             </div>
             <div className="">
                 <h3>React</h3>
-                <p></p>
+                <img src={reactLogo}  alt="CSS logo" className="react logo"/>
             </div>
             <div className="">
                 <h3>.NET</h3>
-                <p></p>
+                <img src={dotNet}  alt="CSS logo" className="react logo"/>
             </div>
             <div className="">
-                <h3>Illustrator</h3>
-                <p></p>
+                <h3>Adobe Suite</h3>
+                <img src={Adobe}  alt="CSS logo" className="react logo"/>
             </div>
             <div className="">
-                <h3>PhotoShop</h3>
-                <p></p>
+                <h3>Bootstrap</h3>
+                <img src={Bootstrap}  alt="CSS logo" className="react logo"/>
             </div>
             <div className="">
-                <h3>Netlify</h3>
-                <p></p>
+                <h3>Node</h3>
+                <img src={Node}  alt="CSS logo" className="react logo"/>
             </div>
             <div className="work-footer">
                 <h3>Footer</h3>
